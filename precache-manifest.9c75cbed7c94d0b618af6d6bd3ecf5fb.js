ec33082fe3684be009822b4f72ee3d2121d206e0
Denys Plietnikov <givisimo@gmail.com>
2019-12-21 19:38:44 +0200

    Updates

---
+++ precache-manifest.9c75cbed7c94d0b618af6d6bd3ecf5fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fe8edff042eb00ee12c4382575970b7",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "a2b02f6a969603b7741e",
+    "url": "/goit-react-hw-03-image-finder/static/css/2.f5298653.chunk.css"
+  },
+  {
+    "revision": "2ed3b38dce8437a08a5f",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.56ea447f.chunk.css"
+  },
+  {
+    "revision": "a2b02f6a969603b7741e",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.da9a76ca.chunk.js"
+  },
+  {
+    "revision": "2ed3b38dce8437a08a5f",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.85b599b8.chunk.js"
+  },
+  {
+    "revision": "8b81b8dd8a3d0626f0a9",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.c7d46ad0.js"
+  }
+]);
